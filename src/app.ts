402669a4d398d387309f255a001890ae26691fce
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-05-08 23:19:12 +0300

    feat: add nodemon for dev server restarting

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,32 @@
+import { createServer, Server } from "node:http"
 export default class App {
-  constructor() {
-  }
+  constructor() {}
 
   start() {
-    console.log("App started");
+    const server = createServer((req, res) => {
+      console.log(req.url)
+      console.log(req.method)
+      res.statusCode = 400
+      res.write("")
+      res.end()
+    })
+
+    server.listen(4000)
+    server.on("connect", () => console.log("Connected"))
+    process.on("SIGINT", () => this.stop(server))
+    process.on("exit", () => this.stop(server))
+    process.on("SIGTERM", () => this.restart())
   }
 
-  stop() {
+  stop(server: Server) {
+    server.close()
+    console.log("Server stopped")
+  }
 
+  restart() {
+    console.log("Server restarting...")
+    setTimeout(() => {
+      console.log("Server restarted")
+    }, 1000)
   }
 }
