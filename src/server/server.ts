635ef56e4c3f42a3ab7374fc60332abb01f25091
Dmitrii Samsonenko <samsondm97@gmail.com>
2025-05-10 02:24:01 +0300

    refactor: split server and response handling logic

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,17 +7,17 @@ import {
 import Router from "../routing/router"
 import sendResponse from "../utils/send-response"
 import HttpError from "../errors/http-error"
-import TCRUDMethod from "../types/crud-method"
+import RequestHandler from "./resuest-handler"
 
 export default class Server {
-  routers: Router[]
   server: HttpServer
   port: number
+  requestHandler: RequestHandler
 
   constructor(port: number) {
-    this.routers = []
     this.server = createServer()
     this.port = port
+    this.requestHandler = new RequestHandler([])
   }
 
   start() {
@@ -27,12 +27,12 @@ export default class Server {
   }
 
   addRouter(newRouter: Router) {
-    this.routers.push(newRouter)
+    this.requestHandler.routers.push(newRouter)
   }
 
   async routeRequest(req: IncomingMessage, res: ServerResponse) {
     try {
-      await this.handleRequest(req, res)
+      await this.requestHandler.handleRequest(req, res)
     } catch (err) {
       if (err instanceof HttpError) {
         sendResponse({ res, code: err.code, message: err.message })
@@ -42,29 +42,6 @@ export default class Server {
         sendResponse({ res, code: 500, message: String(err) })
       }
     }
-  }
-
-  async handleRequest(req: IncomingMessage, res: ServerResponse) {
-    const path = req.url ? req.url : "/"
-    const method = req.method?.toUpperCase() as TCRUDMethod | undefined
-
-    if (!method) {
-      throw new HttpError(405, "Method not allowed")
-    }
-
-    for (const router of this.routers) {
-      if (!router.match(path)) continue
-
-      const match = router.findRoute(path, method)
-
-      if (match) {
-        const { route, params } = match
-        const responseContent = await route.handler(req, res, params)
-        sendResponse(responseContent)
-        return
-      }
-    }
-    throw new HttpError(404, `Page not found for path ${path}`)
   }
 
   close() {
--- src/utils/send-response.ts
+++ src/utils/send-response.ts
@@ -8,6 +8,7 @@ export interface ISendResponse {
 
 const sendResponse = ({ res, code, message }: ISendResponse) => {
   res.statusCode = code
+  res.setHeader("Content-Type", "application/json")
   res.write(message)
   res.end()
 }
